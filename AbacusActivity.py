cd1d44894b317495e23958f508d79d4c3c352a26
Gonzalo Odiard <godiard@gmail.com>
2014-05-20 14:12:13 -0300

    Do not switch to a abacus already selected
    This change avoid one redraw at startup in the default case

--- AbacusActivity.py
+++ AbacusActivity.py
@@ -282,6 +282,10 @@ class AbacusActivity(activity.Activity):
     def _switch_modes(self, abacus):
         ''' Display the selected abacus '''
         _logger.debug('switching modes to %s', abacus)
+        if abacus == self.abacus.mode.name:
+            _logger.debug('do not switch already in the same mode')
+            self.get_window().set_cursor(None)
+            return
         # Save current value
         value = int(float(self.abacus.mode.value()))
         if abacus == 'custom' and self.abacus.custom is None:
